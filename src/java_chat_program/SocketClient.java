5f93ad41e11cdea1d3c7c7b39308bd5a973eeed0
Bradley-Cunningham <bradleycunningham0@gmail.com>
2016-07-28 14:41:25 +1200

    Made so only sends messages then stops/ Runs in thread

--- src/java_chat_program/SocketClient.java
+++ src/java_chat_program/SocketClient.java
@@ -5,6 +5,14 @@
  */
 package java_chat_program;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Bradley
@@ -24,5 +32,29 @@ public class SocketClient extends Thread {
         this.name = name;
     }
     
-    
+    @Override
+    public void run() {
+        try {
+            Socket socket = null;
+            PrintWriter out = null;
+            try {
+                socket = new Socket(ip,port);
+                out = new PrintWriter(socket.getOutputStream(), true);
+                System.out.println("Client Connected and ready to send");
+            }   catch (IOException ex) {
+                System.out.println("Failed to setup socket");
+                System.out.println(ex.getMessage());
+            }
+            //Send message
+            out.print(name + ": " + message + "\n");
+            out.flush();
+           //Close Socket Client
+            out.close();
+            socket.close();
+  
+        }   catch (IOException ex) {
+            System.out.println("Message failed to send");
+            System.out.print(ex.getMessage());
+        }     
+    }
 }
